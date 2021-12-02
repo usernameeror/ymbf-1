60d74b34233e6b89a362cd94c09c488a8bb34df9
Aang Ardiansyah-XD <92802033+AngCyber@users.noreply.github.com>
2021-12-02 16:20:38 +0700

    Add files via upload

--- Multi_BF.py
+++ Multi_BF.py
@@ -99,7 +99,7 @@ lo_ngentod = '1714009362122228'
 def hasil(ok,cp):
     if len(ok) != 0 or len(cp) != 0:
         print '\n\n %s[%s•%s] Crack selesai sayang..'%(N,K,N)
-        print '\n\n %s[%s•%s] Terimakasih telah menggunakan script ini...
+        print '\n\n %s[%s•%s] Terimakasih telah menggunakan script ini...'%(N,K,N)
         print '\n\n [%s•%s] total OK : %s%s%s'%(O,N,H,str(len(ok)),N)
         print ' [%s•%s] total CP : %s%s%s'%(O,N,K,str(len(cp)),N);exit()
     else:
